df35db5d0d432dc15a294c2c2f73ffa238140a8e
heejiyang <dianoheeji@naver.com>
2022-09-17 01:42:22 +0900

    second

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,8 @@
 // js file
+const modal = document.getElementById("cont-modal");
+const modalOpen = document.getElementById("modal-open-btn")
+
+modalOpen,addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.classList.add('active');
+})
