d3c6ad00b687c6d1f822845543f7e35b69c4ebeb
heejiyang <dianoheeji@naver.com>
2022-11-27 23:34:03 +0900

    css수정

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-// js file
-const modal = document.getElementById("cont-modal");
-const modalOpen = document.querySelector(".modal-open-btn")
+// 모달창
+const modal = document.getElementById('#modal-wrap');
+const modalOpen = document.querySelector('.btn-go');
+const modalClose = document.querySelector('.btn-close');
+
 
 modalOpen.addEventListener('click', (event) => {
     event.preventDefault();
     modal.classList.add('active');
-})
+});
+
+modalClose.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.classList.remove('active');
+});
